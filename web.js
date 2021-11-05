d6318a1855311a459d8d90d61f6fe8e53d83a451
JinwooChoi Laptop <imjinwoo98@gmail.com>
2021-11-04 22:35:34 -0700

    mobile test7

--- web.js
+++ web.js
@@ -38,16 +38,16 @@ function onScroll(event){
 
 
 function clickEffect(e){
-	var d=document.createElement("div");
+  var d=document.createElement("div");
   var d2=document.createElement("div");
-	d.className="clickEffect1";
+  d.className="clickEffect1";
   d2.className="clickEffect2"
-	d.style.top=e.clientY+"px";d.style.left=e.clientX+"px";
+  d.style.top=e.clientY+"px";d.style.left=e.clientX+"px";
   d2.style.top=e.clientY+"px";d2.style.left=e.clientX+"px";
 
-	document.body.appendChild(d);
+  document.body.appendChild(d);
   document.body.appendChild(d2);
-	d.addEventListener('animationend',function(){d.parentElement.removeChild(d);}.bind(this));
+  d.addEventListener('animationend',function(){d.parentElement.removeChild(d);}.bind(this));
   d2.addEventListener('animationend',function(){d2.parentElement.removeChild(d2);}.bind(this));
 }
 document.addEventListener('click',clickEffect);
